3e471cd185a568c0fe21b0389294dc5cebf8fe01
GinaKim <wls9416@gmail.com>
2024-01-30 18:50:05 +0900

    UPDATED: new tab

--- ch01.py
+++ ch01.py
@@ -22,7 +22,7 @@ def main():
 
     aisles, departments, product_prior, product_train, orders, products = load_data()
     
-    tab1, tab2 = st.tabs(["order", "products"])
+    tab1, tab2, tab3 = st.tabs(["order", "products", "category"])
 
     with tab1:
         st.header("When do people order?")
@@ -41,11 +41,38 @@ def main():
         #st.table(order_product.head(5))
         order_product_aisles = pd.merge(order_product, aisles, on = 'aisle_id', how = 'left')
         order_product_aisles_dep = pd.merge(order_product_aisles, departments, on='department_id', how='left')
-        # st.table(order_product_aisles_dep.head(3))
 
         cnt_srs = order_product_aisles_dep['product_name'].value_counts().reset_index().head(20)
         cnt_srs.columns = ['product_name', 'frequency_count']
         st.table(cnt_srs)
+
+    with tab3:
+        st.header("BestSellers by department")
+
+        result = order_product_aisles_dep.groupby(['department', 'product_name']).size().reset_index(name='frequency_count')
+        result = result.sort_values(['department', 'frequency_count'], ascending=[True, False])
+
+        
+        option = st.selectbox('Which department?', departments['department'] )
+        st.write(option)
+
+
+        if option:
+            df = result.loc[result['department'] == option, :]
+            st.table(df.reset_index(drop=True))
+
+        else:
+            st.write("Please select a department.")
+            
+
+
+
+
+        
+
+
+
+    
 
 if __name__ == "__main__":
     main()
